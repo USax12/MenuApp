19f85eb8f67b4f12e2ed98937de94c3c33a8b2cd
utkarshsaxena <utkarshsaxena@IN-5CD127F7Z7.Nagarro.local>
2024-02-26 17:31:11 +0530

    Menu system changes

--- restaurant/src/main/java/com/example/restaurant/repository/MenuRepository.java
+++ restaurant/src/main/java/com/example/restaurant/repository/MenuRepository.java
@@ -1,5 +1,7 @@
 package com.example.restaurant.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.example.restaurant.model.MenuItem;
 @Repository
 public interface MenuRepository extends JpaRepository<MenuItem, Long> {
 
+	 Optional<MenuItem> findByName(String itemName);
 }
--- restaurant/src/main/java/com/example/restaurant/utils/DataSeeder.java
+++ restaurant/src/main/java/com/example/restaurant/utils/DataSeeder.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.example.restaurant.model.MenuItem;
 import com.example.restaurant.repository.MenuRepository;
 
 @Component
@@ -22,16 +23,16 @@ public class DataSeeder implements CommandLineRunner {
 
 	private void seedMenuData() {
 		// Sample menu data
-		/*
-		 * MenuItem burger = new MenuItem("Burger", "Food", 10.99); MenuItem wine = new
-		 * MenuItem("Wine", "Alcoholic", 15.99); MenuItem coke = new MenuItem("Coke",
-		 * "Non-Alcoholic", 2.99);
-		 */
+
+		MenuItem burger = new MenuItem("Burger", "Food", 10.99);
+		MenuItem wine = new MenuItem("Wine", "Alcoholic", 15.99);
+		MenuItem coke = new MenuItem("Coke", "Non-Alcoholic", 2.99);
 
 		// Save menu items to the database
-		/*
-		 * menuRepository.save(burger); menuRepository.save(wine);
-		 * menuRepository.save(coke);
-		 */
+
+		menuRepository.save(burger);
+		menuRepository.save(wine);
+		menuRepository.save(coke);
+
 	}
 }
